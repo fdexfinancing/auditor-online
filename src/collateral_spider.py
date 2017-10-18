e279a62aaa9f2e697e6cf69e076a153f9bdfa73e
Murillo <mmbfreitas@hotmail.com>
2017-10-18 10:59:26 -0200

    add array in sites of scrapy

--- src/collateral_spider.py
+++ src/collateral_spider.py
@@ -3,7 +3,9 @@ import os
 
 class CollateralSpider(scrapy.Spider):
     name = 'CollateralSpider'
-    start_urls = os.environ['SITE_URL']
+    start_urls = [os.environ['SITE_URL']]
 
     def parse(self, response):
+        print('inicio')
+        print(response)
         return
