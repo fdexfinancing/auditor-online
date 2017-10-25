974a3d506d0cc1b021138a9e8e3e6eeb70d23d9c
Murillo <mmbfreitas@hotmail.com>
2017-10-25 12:07:06 -0200

    go throgh result pages

--- src/collateral_spider.py
+++ src/collateral_spider.py
@@ -1,11 +1,36 @@
 import scrapy
 import os
+import datetime
 
 class CollateralSpider(scrapy.Spider):
     name = 'CollateralSpider'
-    start_urls = [os.environ['SITE_URL']]
+    start_urls = ['https://www.vivareal.com.br/venda/']
 
     def parse(self, response):
-        print('inicio')
-        print(response)
+        print(response.url)
+        print("----------")
+        site_name = self.get_site_name(response.url)
+        cur_date = str(datetime.datetime.now().year) + '_' + str(datetime.datetime.now().month)
+        path = 'site/' + site_name + '/' + cur_date + '/'
+        page = 1;
+
+        for next_page in response.css('a.js-paginate-next'):
+            link = next_page.xpath('@href').extract()[0]
+            link = link[link.find('?'):]
+            page += 1
+
+            if page <= 2:
+                yield response.follow(response.url + link, self.parse)
+            else:
+                print('foi')
+        # file = open("../site/index.html","w")
+        # file.write(response.body.decode("utf-8"))
+        # file.close()
         return
+
+    def get_site_name(self, url):
+        site_url = url
+        init = site_url.find('www.')
+        site_name = site_url[init + 4:-1]
+        end = site_name.find('.')
+        return site_name[0:end]
